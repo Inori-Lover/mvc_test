db00620220247769710f74bf0ab75379e88c16ec
inori-lover <337938188@qq.com>
2021-05-31 22:19:47 +0800

    feat: 过滤

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,4 +1,12 @@
-import { FC, FormEvent, memo, useCallback, useState } from "react";
+import {
+  FC,
+  FormEvent,
+  ChangeEventHandler,
+  memo,
+  useCallback,
+  useState,
+  useMemo,
+} from "react";
 import { List } from "./components/list";
 import { I_Item } from "./types";
 import { id } from "./util";
@@ -31,17 +39,77 @@ const Input: FC<InputProps> = memo(function Input({ onSubmit }) {
 });
 
 export const Entry = memo(function Entry() {
-  const [taskList, setTaskList] = useState<I_Item[]>([]);
+  const [taskList, setTaskList] = useState<I_Item[]>([
+    { key: id(), name: "pending", done: false },
+    { key: id(), name: "done", done: true },
+  ]);
 
   const submitHandle = useCallback((name: string) => {
     setTaskList((pre) => [...pre, { key: id(), name, done: false }]);
   }, []);
 
+  const [displayPart, setDisplayPart] = useState("all");
+  const switchDisplayPartHandle: ChangeEventHandler<HTMLInputElement> = (
+    evt
+  ) => {
+    setDisplayPart(evt.currentTarget.value);
+  };
+
+  const dataList = useMemo(() => {
+    switch (displayPart) {
+      case "all": {
+        return taskList;
+      }
+      case "pending": {
+        return taskList.filter((item) => !item.done);
+      }
+      case "done": {
+        return taskList.filter((item) => item.done);
+      }
+      default: {
+        return [];
+      }
+    }
+  }, [displayPart, taskList]);
+
   return (
-    <>
-      <head>todo mvc</head>
-      <Input onSubmit={submitHandle} />
-      <List data={taskList} onChange={setTaskList} />
-    </>
+    <section>
+      <h1>todo mvc</h1>
+      <div>
+        <Input onSubmit={submitHandle} />
+        <label>
+          <input
+            type="radio"
+            name="display-part"
+            onChange={switchDisplayPartHandle}
+            value="all"
+            checked={displayPart === "all"}
+          />{" "}
+          显示全部
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="display-part"
+            onChange={switchDisplayPartHandle}
+            value="pending"
+            checked={displayPart === "pending"}
+          />{" "}
+          显示未完成
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="display-part"
+            onChange={switchDisplayPartHandle}
+            value="done"
+            checked={displayPart === "done"}
+          />{" "}
+          显示已完成
+        </label>
+        <hr />
+        <List data={dataList} onChange={setTaskList} />
+      </div>
+    </section>
   );
 });
